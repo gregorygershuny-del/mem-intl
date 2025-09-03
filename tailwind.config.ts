32630ae0682dd1ce6760393fcb46650fb8828543
Gregory Gershuny <gregorygershuny@gmail.com>
2025-09-02 19:49:04 -0500

    Update NSC page with animated service cards

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+﻿import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
